fd6eab0062ccdc1cf81f5b66ba62108fd9b75c77
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2024-05-06 11:39:55 +0200

    clustering done

---
+++ prueba.py
@@ -0,0 +1,26 @@
+import nbformat
+from nbconvert.preprocessors import ExtractOutputPreprocessor
+from IPython.display import display
+import base64
+import os
+
+def extract_images_from_notebook(notebook_path, output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    
+    with open(notebook_path, 'r') as f:
+        notebook_content = nbformat.read(f, as_version=4)
+
+    for idx, cell in enumerate(notebook_content['cells'], start=1):
+        if cell['cell_type'] == 'code' and 'outputs' in cell:
+            for output in cell['outputs']:
+                if 'data' in output and 'image/png' in output['data']:
+                    image_data = output['data']['image/png']
+                    image_data_decoded = base64.b64decode(image_data)
+                    with open(os.path.join(output_dir, f"output_image_{idx}.png"), "wb") as img_file:
+                        img_file.write(image_data_decoded)
+
+if __name__ == "__main__":
+    notebook_path = "clustering.ipynb"
+    output_directory = "clustering_images"
+    extract_images_from_notebook(notebook_path, output_directory)
